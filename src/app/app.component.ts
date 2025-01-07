5526f6f66c6d4a3c9e75c88193eb475fbe86ab5e
Kenneth <keeentt19@gmail.com>
2025-01-07 01:15:10 -0300

    component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { SimpleQuizComponent } from './simple-quiz/simple-quiz.component';
 
 @Component({
   selector: 'app-root',
