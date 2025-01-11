e7b32d00802d3375dde36ae9636fdf0414cbc325
Kenneth <keeentt19@gmail.com>
2025-01-10 23:43:02 -0300

    rotas ajustadas e home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,16 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   {
     path: 'home',
-    component: AppComponent,
+    loadComponent: () =>
+      import('./home/home.component').then((c) => c.HomeComponent),
   },
 
   {
-    path: '',
+    path: 'quiz',
     loadComponent: () =>
       import('./simple-quiz/simple-quiz.component').then(
         (c) => c.SimpleQuizComponent
